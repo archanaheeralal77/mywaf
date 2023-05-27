4e285706bd95d3008556452d347f48b546e2796d
archanaheeralal77 <127080874+archanaheeralal77@users.noreply.github.com>
2023-05-27 09:44:23 +0530

    Add files via upload

---
+++ EvalWAF.py
@@ -0,0 +1,46 @@
+import os 
+import sys
+
+
+os.system("curl -svo /dev/null "http://localhost/dvwa//test?<script>alert("ArchanaTest")</script>"")
+
+os.system("curl -svo /dev/null "http://localhost?archana=heeralal'+OR+1=1--"")
+
+
+os.system("curl -svo /dev/null "http://localhost?heeralal=1|whoami"")
+
+
+
+os.system("curl -svo /dev/null "http://localhost?heeralal=x||ping+-c+10+127.0.0.1||"")
+
+
+os.system("curl -svo /dev/null "http://localhost/../../../../etc/passwd%00"")
+
+
+os.system("curl -svo /dev/null "http://localhost/ldap0/rmi/log4j")
+
+"
+os.system("curl -svo /dev/null "http://localhost/ -H “Cookie: /etc/password/"")
+
+
+
+os.system("curl -svo /dev/null "http://localhost/jndi:ldap://:1389/Exploit"")
+
+
+os.system("curl -svo /dev/null "http://localhost/ -H "User-Agent: jndi:dns://MASKED_IP.1/securityscan-http8085"")
+
+
+os.system("curl -svo /dev/null "http://localhost/ -H [window [location]=jsLocation]"")
+
+
+
+
+
+
+
+
+
+
+
+
+
